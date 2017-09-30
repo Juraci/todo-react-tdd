362f2cb42f73cbf359ca226b1ff785df92407916
Juraci Vieira <juraci.vieira@gmail.com>
2017-09-30 18:40:09 -0300

    failing tests

--- src/App.js
+++ src/App.js
@@ -5,14 +5,22 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <div className='App'>
+        <header className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
+          <h1 className='App-title'>React Todos</h1>
         </header>
-        <p className="App-intro">
-          Hello React
-        </p>
+        <div className='Todo-App'>
+          <form>
+            <input className='todo-description' type='text'/>
+            <input id='add-task' type='submit' value='Add'/>
+          </form>
+          <div className='Todo-List'>
+            <ul>
+              <li className='Todo-Item' state='todo'><input type='checkbox'/>Learn JSX</li>
+            </ul>
+          </div>
+        </div>
       </div>
     );
   }
--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import App from './App';
 import EnzymeSetup from '../configs/EnzymeSetup';
-import { shallow, mount, render } from  'enzyme';
+import { mount } from  'enzyme';
 
 EnzymeSetup();
 
-it('renders Hello React', () => {
+it('renders the title', () => {
   const wrapper = mount(<App/>);
-  expect(wrapper.find(".App-intro").text()).toEqual("Hello React");
+  expect(wrapper.find(".App-title").text()).toEqual("React Todos");
+});
+
+it('adding "Learn JSX" task', () => {
+  const wrapper = mount(<App/>);
+  expect(wrapper.find('.Todo-Item').length).toBe(0);
+  wrapper.find('.todo-description').simulate('input', { target: { value: 'Learn JSX' } });
+  wrapper.find('#add-task').simulate('click');
+  expect(wrapper.find('.Todo-Item').length).toBe(1);
+  expect(wrapper.find('.Todo-Item').first().text()).toEqual('Learn JSX');
+});
+
+it('marking a task as completed', () => {
+  const wrapper = mount(<App/>);
+  wrapper.find('.todo-description').simulate('input', { target: { value: 'Learn JSX' } });
+  wrapper.find('#add-task').simulate('click');
+  expect(wrapper.find('.Todo-Item').length).toBe(1);
+  wrapper.find('.Todo-Item input').simulate('click');
+  expect(wrapper.find('.Todo-Item').prop('state')).toEqual('completed');
 });
