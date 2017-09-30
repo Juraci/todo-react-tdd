34804135aead0258e25cf55d3750f05282540c36
Juraci Vieira <juraci.vieira@gmail.com>
2017-09-30 17:51:45 -0300

    Hello React, passing

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import { shallow, mount, render } from  'enzyme';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+configure({ adapter: new Adapter() });
+
+it('resnders Hello React', () => {
+  const wrapper = mount(<App/>);
+  expect(wrapper.find(".App-intro").text()).toEqual("Hello React");
 });
