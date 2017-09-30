49b544f50fd679b8a7e114e2853df0074536d486
Juraci Vieira <juraci.vieira@gmail.com>
2017-09-30 18:00:35 -0300

    fixing typo

--- src/App.test.js
+++ src/App.test.js
@@ -5,7 +5,7 @@ import { shallow, mount, render } from  'enzyme';
 
 EnzymeSetup();
 
-it('resnders Hello React', () => {
+it('renders Hello React', () => {
   const wrapper = mount(<App/>);
   expect(wrapper.find(".App-intro").text()).toEqual("Hello React");
 });
